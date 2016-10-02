bb85b8672b4c0259c89ce7fb5365597330ba584f
Gustavo Samico <gustavo.samico@gmail.com>
2016-10-02 02:10:14 -0300

    Renderizando lista

--- src/App.js
+++ src/App.js
@@ -1,18 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import * as firebase from 'firebase';
 
 class App extends Component {
-  render() {
+
+  constructor ()
+  {
+      super();
+      this.state = {
+        accounts : {}
+      };
+  }
+
+  componentDidMount ()
+  {
+    const accounts = firebase.database().ref().child('accounts');
+    const __this = this;
+
+    accounts.on ( 'value', function ( snap )
+    {
+      console.log('Dados recebidos', snap.val());
+      __this.setState({
+        accounts: snap.val()
+      });
+    });
+  }
+
+  render()
+  {
+    const __this = this;
+    let accountsList = Object.keys(this.state.accounts).map ( function ( key )
+    {
+        return <p key={key}>{__this.state.accounts[key].name}, {__this.state.accounts[key].cnpj}</p>;
+    });
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className="App" key="main">
+        {accountsList}
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import './index.css';
+import * as firebase from 'firebase';
+
+var config = {
+  apiKey: "AIzaSyCuVeQIix7oHbKwE79XPyQHlOsffcCY62M",
+  authDomain: "brasilforce-eb7da.firebaseapp.com",
+  databaseURL: "https://brasilforce-eb7da.firebaseio.com",
+  storageBucket: "brasilforce-eb7da.appspot.com",
+  messagingSenderId: "422887134618"
+};
+firebase.initializeApp(config);
 
 ReactDOM.render(
   <App />,
